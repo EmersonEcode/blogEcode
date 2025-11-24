4c4b423ceaeab4c258bc142de82a2507e44f9564
EmersonEcode <=>
2025-11-24 16:13:31 -0500

    aplicando anuncios

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://ecode-api.onrender.com/api' // <-- COLOQUE A URL DA SUA API AQUI
+  apiUrl: 'https://ecode-api.onrender.com/api'
 }
